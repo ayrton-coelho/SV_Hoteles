feebf7e7434f1b7d2dd12abe64b956f7c0f17067
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-01-02 15:02:08 -0300

    done: form input into database

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sv_hoteles_input (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id CHAR(36) PRIMARY KEY,
     check_ VARCHAR(9) NOT NULL,
     hora VARCHAR(9) NOT NULL,
     fecha VARCHAR(10) NOT NULL,
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
+const { v4: uuidv4 } = require('uuid');
 
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'ayrton',
+    password: 'password',
+    database: 'sv_hoteles'
+});
+
+// set and use express evironment
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public'))); // to serve static files
@@ -15,16 +25,22 @@ app.get('/hotels', (req, res) => {
     res.render('hotel_index');
 });
 
-let form_input = [];
-
 app.post('/hotels', (req, res) => {
-    form_input.push(req.body);
-    console.log(req.body);
-    console.log(form_input);
+    let input = req.body;
+    input['id'] = uuidv4();
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = "INSERT INTO sv_hoteles_input (id, check_, hora, fecha, nro_habitacion, nro_personas, origen)\
+        VALUES (?, ?, ?, ?, ?, ?, ?);";
+        con.query(sql, [input.id, input.check, input.hora, input.fecha, input.nro_habitacion, input.nro_personas, input.desde], function (err, result) {
+            if (err) throw (err)
+        });
+    });
+    console.log(input.id);
 });
 
 app.get('/transport', (req, res) => {
-    res.render('transporte_index', { form_input });
+    res.render('transporte_index');
 });
 
 app.listen(3000, 'localhost', () => {
