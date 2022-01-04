3038ee1d3debb6a7955cb20b31be5dbe53083ec9
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-01-04 01:52:11 -0300

    x

--- SQLfiles/create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS sv_hoteles_input (
-    id CHAR(36) PRIMARY KEY,
-    check_ VARCHAR(9) NOT NULL,
-    hora VARCHAR(9) NOT NULL,
-    fecha VARCHAR(10) NOT NULL,
-    nro_habitacion INT NOT NULL,
-    nro_personas INT NOT NULL,
-    origen VARCHAR(30) NOT NULL
-);
