055f08a49b29522f413b2c19b2f5510225e85515
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-01-10 04:37:44 -0300

    done: hora pickup

--- index.js
+++ index.js
@@ -40,22 +40,102 @@ app.get('/hotels', (req, res) => {
 
 // hotel form POST method
 app.post('/hotels', (req, res) => {
-    let input = req.body;
-    input['id'] = uuidv4();
-    const created_at = new Date();
-    let sql = "";
-    if (input.check == 'check_in') {
-      sql = "INSERT INTO sv_hotel_in (id, hora_creacion, hora_de_vuelo, fecha, nro_habitacion, nro_personas, puerto)\
-        VALUES (?, ?, ?, ?, ?, ?, ?);";
-    } else {
-      sql = "INSERT INTO sv_hotel_out (id, hora_creacion, hora_de_vuelo, fecha, nro_habitacion, nro_personas, puerto)\
-        VALUES (?, ?, ?, ?, ?, ?, ?);";
-    }
+  // objeto formulario input
+  let input = req.body;
+  // unique id
+  input['id'] = uuidv4();
+  // hora y fecha de creacion
+  const created_at = new Date();
+  // enviar query a mysqldb
+  let sql = "";
+  if (input.check == 'check_in') {
+    // ARRIBOS
+    sql = "INSERT INTO sv_hotel_in (id, hora_creacion, hora_de_vuelo, fecha, nro_habitacion, nro_personas, puerto)\
+      VALUES (?, ?, ?, ?, ?, ?, ?);";
     db.query(sql, [input.id, created_at, input.hora, input.fecha, input.nro_habitacion, input.nro_personas, input.puerto], function (err, result) {
-            if (err) throw err;
+      if (err) throw err;
     });
-    res.redirect('/hotels');
-    console.log(input.id);
+  } else {
+    // PARTIDAS
+    // calcular hora -3.5 para pickup ------------------->
+    let hora_vuelo = input.hora.split(":");
+    let h = parseInt(hora_vuelo[0]);
+    let m = parseInt(hora_vuelo[1]);
+    let hour_flag = 0;
+    let day_flag = 0;
+    if (m >= 30) {
+      m = m - 30;
+    } else {
+      m = m + 30;
+      hour_flag = 1;
+    }
+    if (h < 3) {
+      hour_flag == 1 ? h = h + 24 - 4 : h = h + 24 - 3; // check if hour flag is on
+      day_flag = 1; // activate day flag
+    } else if (h == 3) {
+      hour_flag == 1 ? h = 23 : h = 0; // check if hour flag is on
+      if (h == 23) {
+        day_flag = 1; // activate day flag
+      }
+    } else {
+      hour_flag == 1 ? h = h - 4 : h = h - 3; // hour bigger than 4, check if hour_flag is on
+    }
+    // formar hora pickup en string
+    let h_to_str = "";
+    let m_to_str = "";
+    let pickup = "";
+    if (h <= 9) {
+      h_to_str = "0" + h.toString();
+    } else {
+      h_to_str = h.toString();
+    }
+    if (m <= 9) {
+      m_to_str = "0" + m.toString();
+    } else {
+      m_to_str = m.toString();
+    }
+    pickup = h_to_str + ":" + m_to_str; // hora pickup lista -------------->
+    // finalmente, checkear flag de fecha
+    if (day_flag == 1) {
+      const fecha = input.fecha.split("-", 3);
+      let dia = parseInt(fecha[2]);
+      let mes = parseInt(fecha[1]);
+      let año = parseInt(fecha[0]);
+      let month_flag = 0;
+      if (dia == 1) {
+        mes = mes - 1;
+        month_flag = 1;
+        if (month_flag == 1) {
+          if (mes == 0) {
+            mes = 12;
+            año = año - 1;
+          }
+        }
+        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+          dia = 31;
+        }
+        else if (mes == 2) {
+          dia = 28;
+        }
+        else {
+          dia = 30;
+        }
+      } else {
+        dia = dia - 1;
+      }
+      pickup = pickup + " (" + (dia).toString() + "/" + mes.toString() + "/" + año.toString() + ")";
+      console.log("Fecha: " + input.fecha + " " + input.hora);
+      console.log("Pickup: " + pickup);
+    }
+    // Enviar query con pickup
+    sql = "INSERT INTO sv_hotel_out (id, hora_creacion, hora_de_vuelo, hora_pickup, fecha, nro_habitacion, nro_personas, nro_valijas, puerto)\
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    db.query(sql, [input.id, created_at, input.hora, pickup, input.fecha, input.nro_habitacion, input.nro_personas, input.nro_valijas, input.puerto], function (err, result) {
+      if (err) throw err;
+    });
+  }
+  res.redirect('/hotels');
+  console.log(input.id);
 });
 
 // render transport page
